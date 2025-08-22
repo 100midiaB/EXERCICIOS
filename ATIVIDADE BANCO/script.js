c81e56bdd5761b1ba4ea87e27f91c74ba03b437b
João Pedro <joao.p.araujo29@aluno.senai.br>
2025-08-22 16:50:39 -0300

    BANCO

---
+++ ATIVIDADE BANCO/script.js
@@ -0,0 +1,11 @@
+var senha = prompt("Digite a sua senha")
+var saldo = 2000
+
+if(senha =="333"){
+    console.log("Senha correta")
+}
+
+else if(senha !="333"){
+    console.log("senha incorreta")
+}
+
